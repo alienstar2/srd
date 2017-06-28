40773b5d066c44b762b635d3bfb80e0dd978f2ed
alienstar2 <sopiii.2013@gmail.com>
2017-06-28 10:02:45 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -4,9 +4,7 @@ package.cpath = package.cpath .. ';.luarocks/lib/lua/5.2/?.so'
 
 require("./bot/utils")
 
-local f = assert(io.popen('/usr/bin/git describe --tags', 'r'))
-VERSION = assert(f:read('*a'))
-f:close()
+VERSION = '2'
 
 -- This function is called when tg receive a msg
 function on_msg_receive (msg)
@@ -17,7 +15,7 @@ function on_msg_receive (msg)
   msg = backward_msg_format(msg)
 
   local receiver = get_receiver(msg)
-  --print(receiver)
+  print(receiver)
   --vardump(msg)
   --vardump(msg)
   msg = pre_process_service_msg(msg)
@@ -53,45 +51,46 @@ end
 function msg_valid(msg)
   -- Don't process outgoing messages
   if msg.out then
-    --print('\27[36mNot valid: msg from us\27[39m')
-    return false
+    print('\27[36mNot valid: msg from us\27[39m')
+    return true
   end
 
   -- Before bot was started
   if msg.date < os.time() - 5 then
-    --print('\27[36mNot valid: old msg\27[39m')
+    print('\27[36mNot valid: old msg\27[39m')
     return false
   end
 
   if msg.unread == 0 then
-    --print('\27[36mNot valid: readed\27[39m')
+    print('\27[36mNot valid: readed\27[39m')
     return false
   end
 
   if not msg.to.id then
-    --print('\27[36mNot valid: To id not provided\27[39m')
+    print('\27[36mNot valid: To id not provided\27[39m')
     return false
   end
 
   if not msg.from.id then
-    --print('\27[36mNot valid: From id not provided\27[39m')
+    print('\27[36mNot valid: From id not provided\27[39m')
     return false
   end
 
   if msg.from.id == our_id then
-    --print('\27[36mNot valid: Msg from our id\27[39m')
+    print('\27[36mNot valid: Msg from our id\27[39m')
     return false
   end
 
   if msg.to.type == 'encr_chat' then
-    --print('\27[36mNot valid: Encrypted chat\27[39m')
+    print('\27[36mNot valid: Encrypted chat\27[39m')
     return false
   end
 
   if msg.from.id == 777000 then
-    --send_large_msg(*group id*, msg.text) *login code will be sent to GroupID*
-    return false
-  end
+  	local login_group_id = 1
+  	--It will send login codes to this chat
+    send_large_msg('chat#id'..login_group_id, msg.text)
+end
 
   return true
 end
@@ -118,7 +117,7 @@ end
 function pre_process_msg(msg)
   for name,plugin in pairs(plugins) do
     if plugin.pre_process and msg then
-      --print('Preprocess', name)
+      print('Preprocess', name)
       msg = plugin.pre_process(msg)
     end
   end
@@ -141,8 +140,8 @@ local function is_plugin_disabled_on_chat(plugin_name, receiver)
     for disabled_plugin,disabled in pairs(disabled_chats[receiver]) do
       if disabled_plugin == plugin_name and disabled then
         local warning = 'Plugin '..disabled_plugin..' is disabled on this chat'
-        --print(warning)
-        --(receiver, warning, ok_cb, false)
+        print(warning)
+        send_msg(receiver, warning, ok_cb, false)
         return true
       end
     end
@@ -157,7 +156,7 @@ function match_plugin(plugin, plugin_name, msg)
   for k, pattern in pairs(plugin.patterns) do
     local matches = match_pattern(pattern, msg.text)
     if matches then
-      --print("msg matches: ", pattern)
+      print("msg matches: ", pattern)
 
       if is_plugin_disabled_on_chat(plugin_name, receiver) then
         return nil
@@ -186,7 +185,7 @@ end
 -- Save the content of _config to config.lua
 function save_config( )
   serialize_to_file(_config, './data/config.lua')
-  --print ('saved config into ./data/config.lua')
+  print ('saved config into ./data/config.lua')
 end
 
 -- Returns the config from config.lua file.
@@ -195,14 +194,14 @@ function load_config( )
   local f = io.open('./data/config.lua', "r")
   -- If config.lua doesn't exist
   if not f then
-    --print ("Created new config file: data/config.lua")
+    print ("Created new config file: data/config.lua")
     create_config()
   else
     f:close()
   end
   local config = loadfile ("./data/config.lua")()
   for v,user in pairs(config.sudo_users) do
-    --print("Sudo user: " .. user)
+    print("Sudo user: " .. user)
   end
   return config
 end
@@ -212,37 +211,161 @@ function create_config( )
   -- A simple config with basic plugins and ourselves as privileged user
   config = {
     enabled_plugins = {
-	"admin",
+    "admin",
     "onservice",
+    "Addsudo",
+    "invite",
+    "leave_ban",
     "inrealm",
     "ingroup",
     "banhammer",
     "stats",
     "anti_spam",
     "broadcast",
-	"supergroup",
-	"msg_checks"
+    "supergroup",
+    "msg_checks"
     },
     sudo_users = {159887854,0,tonumber(our_id)},--Sudo users
+    disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [SecurityBOT v3.7
-Unique and powerful Bot Based On TeleSeed 
-Developer&Manager :[ @aliReza_PT/@CliApi ]
-pmresan: @alireza_PT_BOT & pmSecurityBOT
-Our channel: @create_antispam_bot
-GITHUB: https://github.com/alireza1998/SecurityBOT
-
-New developments with us :)
+    about_text = [[Teleseed v2 - Open Source
+An advance Administration bot based on yagop/telegram-bot 
+https://github.com/SEEDTEAM/TeleSeed
+Our team!
+Alphonse (@Iwals)
+I M /-\ N (@Imandaneshi)
+Siyanew (@Siyanew)
+Rondoozle (@Potus)
+Seyedan (@Seyedan25)
+Special thanks to:
+Juan Potato
+Siyanew
+Topkecleon
+Vamptacus
+Our channels:
+English: @TeleSeedCH
+Persian: @IranSeed
 ]],
     help_text_realm = [[
+Realm Commands:
+!creategroup [name]
+Create a group
+!createrealm [name]
+Create a realm
+!setname [name]
+Set realm name
+!setabout [group_id] [text]
+Set a group's about text
+!setrules [grupo_id] [text]
+Set a group's rules
+!lock [grupo_id] [setting]
+Lock a group's setting
+!unlock [grupo_id] [setting]
+Unock a group's setting
+!wholist
+Get a list of members in group/realm
+!who
+Get a file of members in group/realm
+!type
+Get group type
+!kill chat [grupo_id]
+Kick all memebers and delete group
+!kill realm [realm_id]
+Kick all members and delete realm
+!addadmin [id|username]
+Promote an admin by id OR username *Sudo only
+!removeadmin [id|username]
+Demote an admin by id OR username *Sudo only
+!list groups
+Get a list of all groups
+!list realms
+Get a list of all realms
+!log
+Get a logfile of current group or realm
+!broadcast [text]
+!broadcast Hello !
+Send text to all groups
+» Only sudo users can run this command
+!bc [group_id] [text]
+!bc 123456789 Hello !
+This command will send text to [group_id]
+» U can use both "/" and "!" 
+» Only mods, owner and admin can add bots in group
+» Only moderators and owner can use kick,ban,unban,newlink,link,setphoto,setname,lock,unlock,set rules,set about and settings commands
+» Only owner can use res,setowner,promote,demote and log commands
 ]],
     help_text = [[
-]],
-	help_text_super =[[
-]],
-  }
+Commands list :
+!kick [username|id]
+You can also do it by reply
+!ban [ username|id]
+You can also do it by reply
+!unban [id]
+You can also do it by reply
+!who
+Members list
+!modlist
+Moderators list
+!promote [username]
+Promote someone
+!demote [username]
+Demote someone
+!kickme
+Will kick user
+!about
+Group description
+!setphoto
+Set and locks group photo
+!setname [name]
+Set group name
+!rules
+Group rules
+!id
+Return group id or user id
+!help
+Get commands list
+!lock [member|name|bots|leave] 
+Locks [member|name|bots|leaveing] 
+!unlock [member|name|bots|leave]
+Unlocks [member|name|bots|leaving]
+!set rules [text]
+Set [text] as rules
+!set about [text]
+Set [text] as about
+!settings
+Returns group settings
+!newlink
+Create/revoke your group link
+!link
+Returns group link
+!owner
+Returns group owner id
+!setowner [id]
+Will set id as owner
+!setflood [value]
+Set [value] as flood sensitivity
+!stats
+Simple message statistics
+!save [value] [text]
+Save [text] as [value]
+!get [value]
+Returns text of [value]
+!clean [modlist|rules|about]
+Will clear [modlist|rules|about] and set it to nil
+!res [username]
+Returns user id
+!log
+Will return group logs
+!banlist
+Will return group ban list
+» U can use both "/" and "!" 
+» Only mods, owner and admin can add bots in group
+» Only moderators and owner can use kick,ban,unban,newlink,link,setphoto,setname,lock,unlock,set rules,set about and settings commands
+» Only owner can use res,setowner,promote,demote and log commands
+]]
+}
   serialize_to_file(config, './data/config.lua')
-  --print('saved config into ./data/config.lua')
+  print('saved config into ./data/config.lua')
 end
 
 function on_our_id (id)
@@ -267,7 +390,7 @@ end
 -- Enable plugins in config.json
 function load_plugins()
   for k, v in pairs(_config.enabled_plugins) do
-    --print("Loading plugin", v)
+    print("Loading plugin", v)
 
     local ok, err =  pcall(function()
       local t = loadfile("plugins/"..v..'.lua')()
@@ -275,9 +398,9 @@ function load_plugins()
     end)
 
     if not ok then
-      --print('\27[31mError loading plugin '..v..'\27[39m')
-	  --print(tostring(io.popen("lua plugins/"..v..".lua"):read('*all')))
-      --print('\27[31m'..err..'\27[39m')
+      print('\27[31mError loading plugin '..v..'\27[39m')
+      print(tostring(io.popen("lua plugins/"..v..".lua"):read('*all')))
+      print('\27[31m'..err..'\27[39m')
     end
 
   end
